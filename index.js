6a63bccff155d8d6f16329bdbd978db7fbc7a1f9
sirojbek0908 <sirojbekyoldashev29@gmail.com>
2024-05-10 01:01:20 +0500

    vazifa1

---
+++ index.js
@@ -0,0 +1,106 @@
+
+// //1-misol
+
+// let a=3;
+//  v=Math.pow(a, 3);
+// console.log(v);
+
+
+// //2-misol
+// let r1=3, r2=7, r3=1,pi=3.14;
+//  s1=pi*r1;
+//  s2=pi*r2;
+//  s3=pi*r3;
+// console.log(s1+" "+s2+" "+s3.toFixed(2));
+
+
+// //3-misol
+
+// let s=8, h=5;
+// a=2*s/h;
+// console.log(a.toFixed(2));
+
+
+// //4-misol
+
+// let r=2, p=3.14;
+
+// s=4*p*r*r;
+
+// console.log(s);
+
+
+// //5- misol
+
+// let a=6,b=6,c=9;
+// p=(a+b+c)/2;
+// console.log(p.toFixed(2));
+
+// //7-misol
+
+// let h=3,r=5,pi=3.14;
+// let v=1/3*h*(r**2)*pi;
+// console.log(v.toFixed(2));
+
+
+// //8-misol
+
+// let v=6,s=2;
+// t=s/v;
+// console.log(t.toFixed(2));
+
+
+// //9-misol
+
+// let h=3,g=9.8;
+// t=h*2/g;
+// console.log(Math.sqrt(t).toFixed(2));
+
+// //10-misol
+
+// let t1=1,v1=Math.pow(10,-3),t2=63072000;
+// v2=(v1*t2)/t1;
+// console.log(v2);
+
+
+// //11-misol
+
+// function toplam(n) {
+//     let toplam = 0;
+//     for (let i = 1; i <= n; i++) {
+//       toplam += i;
+//     }
+//     return toplam;
+//   }
+  
+//   // Örnek kullanım:
+//   console.log(toplam(10)); // kiritilgan songacha bolgan sonlarni qoshadi
+
+// //12-misol
+
+
+// let m=6,g=9.8;
+// p=m*g;
+// console.log(p.toFixed(2));
+
+
+// //13-misol
+
+// let m=8,a=2;
+// f=m*a;
+// console.log(f);
+
+
+// //14-misol
+
+
+// let u=8,r=7;
+// i=u/r;
+// console.log(i.toFixed(3));
+
+
+// //15-misol
+
+// let r1=1,r2=7,r3=3;
+// let a=(r1*r2*r3)/(r2*r3+r1*r3+r2*r1);
+// console.log(a.toFixed(2));
